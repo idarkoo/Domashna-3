ffd4833fd4d3312954d52a78d7177b54ff21f652
idarkoo <48064623+idarkoo@users.noreply.github.com>
2019-03-13 08:56:52 +0100

    Create Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppdomashno.classes
+{
+   public  class Class1
+    {
+        public string Name { get; set; }
+        public string Academy { get; set; }
+        public string Group { get; set; }
+
+        public Class1(string name, string academy, string group)
+        {
+            Name = name;
+            Academy = academy;
+            Group = group;
+
+        }
+    }
+}
